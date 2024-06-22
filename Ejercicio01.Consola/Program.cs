b56d68653913d04f72c8259938f59f3b970d9091
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2024-06-22 12:41:21 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio01.Consola/Program.cs
@@ -0,0 +1,226 @@
+﻿
+using ConsoleTables;
+using System.Text;
+
+namespace Ejercicio01.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcionSeleccionada;
+            int[] numAzar = new int[10];
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("0 - Salir");
+                Console.WriteLine("1 - Generar Vector");
+                Console.WriteLine("2 - Mostrar Vector");
+                Console.WriteLine("3 - Estadísticas");
+                Console.WriteLine("4 - Ordenar Vector");
+                Console.WriteLine("5 - Mostrar Num. Pares");
+                Console.WriteLine("6 - Mostrar Num. Primos");
+                do
+                {
+                    Console.Write("Ingrese selección:");
+                    if (!int.TryParse(Console.ReadLine(), out opcionSeleccionada))
+                    {
+                        Console.WriteLine("Opción mal ingresada!!! Reintentar");
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                } while (true);
+
+                switch (opcionSeleccionada)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        GenerarElementosDelVector(numAzar);
+                        break;
+                    case 2:
+                        MostrarVector(numAzar);
+                        break;
+                    case 3:
+                        Estadisticas(numAzar);
+                        break;
+                    case 4:
+                        OrdenarVector(numAzar);
+                        break;
+                    case 5:
+                        MostrarPares(numAzar);
+                        break;
+                    case 6:
+                        MostrarPrimos(numAzar);
+                        break;
+                    default:
+                        break;
+                }
+                if (opcionSeleccionada==0)
+                {
+                    Console.WriteLine("Fin de la aplicación");
+                    break;
+                }
+            } while (true);
+        }
+
+        private static void MostrarPrimos(int[] numAzar)
+        {
+            Console.Clear();
+            Console.WriteLine("Listado de elementos del vector");
+            if (EstaVacio(numAzar))
+            {
+                Console.WriteLine("Vector vacío!!!");
+                Console.WriteLine("Enter para continuar");
+                Console.ReadLine();
+                return;
+            }
+            var tabla = new ConsoleTable("Pos.", "Nro", "Primo");
+            for (int i = 0; i < numAzar.Length; i++)
+            {
+                if (EsPrimo(numAzar[i]))
+                {
+                    tabla.AddRow(i, numAzar[i], "*");
+
+                }
+                else
+                {
+                    tabla.AddRow(i, numAzar[i], string.Empty);
+
+                }
+            }
+            Console.WriteLine(tabla.ToString());
+            Console.WriteLine("Enter para continuar");
+            Console.ReadLine();
+
+        }
+
+        private static bool EsPrimo(int v)
+        {
+
+            for (int i = 2; i < v / 2; i++)
+            {
+                if (v%i==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static void MostrarPares(int[] numAzar)
+        {
+            Console.Clear();
+            Console.WriteLine("Listado de elementos del vector");
+            if (EstaVacio(numAzar))
+            {
+                Console.WriteLine("Vector vacío!!!");
+                Console.WriteLine("Enter para continuar");
+                Console.ReadLine();
+                return;
+            }
+            var tabla = new ConsoleTable("Pos.", "Nro","Nro. Par");
+            for (int i = 0; i < numAzar.Length; i++)
+            {
+                if (EsPar(numAzar[i]))
+                {
+                    tabla.AddRow(i, numAzar[i],"*");
+
+                }
+                else
+                {
+                    tabla.AddRow(i, numAzar[i], string.Empty);
+
+                }
+            }
+            Console.WriteLine(tabla.ToString());
+            Console.WriteLine("Enter para continuar");
+            Console.ReadLine();
+
+        }
+
+        private static bool EsPar(int v)
+        {
+            return v % 2 == 0;
+        }
+
+        private static void OrdenarVector(int[] numAzar)
+        {
+
+            Array.Sort(numAzar);
+            Console.Clear();
+            Console.WriteLine("Vector Ordenado");
+            MostrarVector(numAzar);
+
+        }
+
+        private static void Estadisticas(int[] numAzar)
+        {
+            int menor = numAzar.Min();
+            int mayor = numAzar.Max();
+            double promedio = numAzar.Average();
+            Console.Clear();
+            Console.WriteLine("Estadísticas");
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Vector de {numAzar.Length} elementos");
+            sb.AppendLine($"Menor elemento: {menor}");
+            sb.AppendLine($"Mayor elemento: {mayor}");
+            sb.AppendLine($"Media de elementos: {promedio}");
+            Console.WriteLine(sb.ToString());
+            EsperaTecla();
+
+        }
+
+        private static void EsperaTecla()
+        {
+            Console.WriteLine("Enter para continuar");
+            Console.ReadLine();
+        }
+
+        private static void MostrarVector(int[] numAzar)
+        {
+            Console.Clear();
+            Console.WriteLine("Listado de elementos del vector");
+            if (EstaVacio(numAzar))
+            {
+                Console.WriteLine("Vector vacío!!!");
+                Console.WriteLine("Enter para continuar");
+                Console.ReadLine();
+                return;
+            }
+            var tabla = new ConsoleTable("Pos.", "Nro");
+            for (int i = 0; i < numAzar.Length; i++)
+            {
+                tabla.AddRow(i, numAzar[i]);
+            }
+            Console.WriteLine(tabla.ToString());
+            Console.WriteLine("Enter para continuar");
+            Console.ReadLine();
+
+        }
+
+        private static bool EstaVacio(int[] numAzar)
+        {
+            return numAzar.All(x => x == 0);
+        }
+
+        private static void GenerarElementosDelVector(int[] numAzar)
+        {
+            Console.Clear();
+            Console.WriteLine("Generando elementos del vector!!!");
+
+            Random r = new Random();
+            for (int i = 0; i < numAzar.Length; i++)
+            {
+                numAzar[i] = r.Next(1, 51);
+            }
+
+            Console.WriteLine("Números generados...");
+            Console.WriteLine("Enter para continuar");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicio02.Consola/Program.cs
@@ -0,0 +1,149 @@
+﻿
+
+
+using ConsoleTables;
+
+namespace Ejercicio02.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? opcionSeleccionada;
+            int[] velocidades = new int[5];
+            do
+            {
+
+                Console.Clear();
+                Console.WriteLine("0 - Salir");
+                Console.WriteLine("1 - Ingresar Velocidades");
+                Console.WriteLine("2 - Mostrar Velocidades");
+                Console.WriteLine("3 - Datos Estadísticos");
+                Console.WriteLine("4 - Marcar Supeiores al Promedio");
+                Console.WriteLine("5 - Ordenar Vector");
+                Console.Write("Ingrese selección:");
+                opcionSeleccionada = Console.ReadLine();
+                switch (opcionSeleccionada)
+                {
+                    case "0":
+                        Console.WriteLine("Fin de la Aplicación");
+                        return;
+                    case "1":
+                        IngresarDatos(velocidades);
+                        break;
+                    case "2":
+                        MostrarVelocidades(velocidades);
+                        break;
+                    case "3":
+                        DatosEstadisticos(velocidades);
+                        break;
+                    case "4":
+                        MarcarSuperioresPromedio(velocidades);
+                        break;
+                    case "5":
+                        OrdenarVector(velocidades);
+                        break;
+                    default:
+                        break;
+                }
+            } while (true);
+        }
+
+        private static void OrdenarVector(int[] velocidades)
+        {
+            Array.Sort(velocidades);
+            MostrarVelocidades(velocidades);
+        }
+
+        private static void MarcarSuperioresPromedio(int[] velocidades)
+        {
+            var promedio = velocidades.Average();
+            Console.Clear();
+            Console.WriteLine("Listado de Velocidades con superiores al promedio");
+            Console.WriteLine($"Promedio........: {promedio:N2}");
+            var tabla = new ConsoleTable("Pos", "Km", "Millas", "Sup. Promedio");
+            for (int i = 0; i < velocidades.Length; i++)
+            {
+                var millas = ConvertirKmMillas(velocidades[i]);
+                if (velocidades[i] > promedio)
+                {
+                    tabla.AddRow(i, velocidades[i], millas, "*");
+
+                }
+                else
+                {
+                    tabla.AddRow(i, velocidades[i], millas,string.Empty);
+                }
+            }
+            Console.WriteLine(tabla.ToString());
+            EsperarTecla("Listado finalizado... Tecla para continuar");
+
+        }
+
+        private static void DatosEstadisticos(int[] velocidades)
+        {
+            Console.Clear();
+            Console.WriteLine("Datos Estadísticos del Vector");
+            var maxVelocidad = velocidades.Max();
+            var minVelocicad=velocidades.Min();
+            var promVelocidad = velocidades.Average();
+            var cantidadInferiorPromedio=velocidades.Count(v=>v<promVelocidad);
+            Console.WriteLine($"Mayor Velocidad: {maxVelocidad}");
+            Console.WriteLine($"Menor Veloclidad: {minVelocicad}");
+            Console.WriteLine($"Promedio........: {promVelocidad}");
+            Console.WriteLine($"Inferiores al Prom: {cantidadInferiorPromedio}");
+            EsperarTecla("Proceso finalizado... tecla para continuar");
+        }
+
+        private static void MostrarVelocidades(int[] velocidades)
+        {
+            Console.Clear();
+            Console.WriteLine("Listado de Velocidades");
+            var tabla = new ConsoleTable("Pos", "Km", "Millas");
+            for (int i = 0; i < velocidades.Length; i++)
+            {
+                var millas = ConvertirKmMillas(velocidades[i]);
+                tabla.AddRow(i, velocidades[i], millas);
+            }
+            Console.WriteLine(tabla.ToString());
+            EsperarTecla("Listado finalizado... Tecla para continuar");
+        }
+
+        private static void IngresarDatos(int[] velocidades)
+        {
+            Console.Clear();
+            Console.WriteLine("Ingreso de Velocidades al vector");
+            for (int i = 0; i < velocidades.Length; i++)
+            {
+                int velocidad;
+                do
+                {
+                    Console.Write($"Ingrese la {i + 1} velocidad:");
+                    if (!int.TryParse(Console.ReadLine(), out velocidad) ||
+                        velocidad < 100 || velocidad > 300)
+                    {
+                        Console.WriteLine("Velocada mal ingresada o fuera de rango [100 -300]");
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                } while (true);
+                velocidades[i] = velocidad;
+            }
+            EsperarTecla("Ingreso finalizado... Presione tecla para continuar");
+        }
+
+        private static void EsperarTecla(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+            Console.ReadLine();
+        }
+
+        private static double ConvertirKmMillas(int v)
+        {
+            return v * 0.6214;
+        }
+    }
+}
